ecc72c8f88de82799a5b465621517bbe97219356
Denis Karmalak <vealo@yandex.ru>
2024-07-07 00:01:02 +0300

    Add class Cat, Eagle, White_Griffin

--- Animals.py
+++ Animals.py
@@ -41,6 +41,9 @@ class Animals:
 class Birds(Animals, Fly):
     description: str = "Птицы"
 
+    def __init__(self, name, age, legs=2, color='Black', sex='men'):
+        Animals.__init__(self, name, age, legs, color, sex)
+
     def move(self, environment='air'):
         if environment == 'air':
             return self.flying()
@@ -75,14 +78,62 @@ class Mammals(Animals, Run, Jump):
 class Griffin(Birds, Mammals):
     description: str = "Это мифическое животное наполовину орел наполовину лев"
 
+    def __init__(self, name, age, legs=4, color='Black', sex='men'):
+        Birds.__init__(self, name, age, legs, color, sex)
     def __str__(self):
         return f'Грифон {self.name}'
 
     def __repr__(self):
         return f'Griffin({self.name, self.age})'
 
+#New class Eagle, Cat, White_Griffin
 
-lst = [Animals('Cat', 11), Birds('Eagle', 1), Mammals('Gazel', 2), Griffin('Вася', 53)]
+class Eagle(Birds):
+
+    description = 'Гордый крылатый хишник с мошным клювом и отличным зрением - Орел'
+
+    def __init__(self, name, age, legs=4, color='Broun', sex='men'):
+        Birds.__init__(self, name, age, legs, color, sex)
+
+    def __str__(self):
+        return f'Орел по имени {self.name}'
+
+    def __repr__(self):
+        return f'Eagle({self.name, self.age})'
+
+class Cat(Mammals):
+
+    description = 'Чудный мелкий пушистик спасающтий людей от грызунов - Кот'
+
+    def __str__(self):
+        return f'Кот по имени {self.name}'
+
+    def __repr__(self):
+        return f'Cat({self.name, self.age})'
+
+class White_Griffin(Griffin):
+
+    description = 'Самый крупный представитель этих легендарных мифических животных - Белый Гриффон'
+
+    def __init__(self, name, age, legs=4, color='White', sex='men'):
+        Birds.__init__(self, name, age, legs, color, sex)
+
+    def __str__(self):
+        return f'Белый гриффон по имени {self.name}'
+
+    def __repr__(self):
+        return f'White_Griffin({self.name, self.age})'
+
+
+lst = [
+       Animals('Cat', 11),
+       Birds('Орел', 2),
+       Mammals('Gazel', 2),
+       Griffin('Вася', 53),
+       Cat('Пушистик', 2),
+       Eagle('Птер', 5),
+       White_Griffin('Лорд', 14)
+       ]
 
 for animal in lst:
     print(animal)
