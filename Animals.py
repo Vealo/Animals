c6953d0de0031ec97e577ff0e7dbe1d757511295
Denis Karmalak <vealo@yandex.ru>
2024-07-10 15:01:05 +0300

    Refactoring; Add func: property; Add decor: @property, @classmethod

--- Animals.py
+++ Animals.py
@@ -14,67 +14,93 @@ class Fly:
     def fly(self) -> str:
         print(f"{self.name} is fly")
 
+#Basic class
 class Animals:
     'Базовый класс от него наследуется все классы животных'
     description: str = "It's a life"
 
     def __init__(self, name: str, age: int) -> None:
-        self.name: str = name
+        self._name: str = name
         self.age: int = age
         self.legs: int = 4
-        self.color: str = 'Black'
+        self._color: str = 'Black'
         self.sex: str = 'men'
+        self._class_name: str = 'Животное'
+        self._class_type: str = 'Animal'
 
-    def get_description(self) -> str:
-        print("Описание класса:", str(self.__class__.description))
+    #Block get / set
+    #Function property
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name='Black'):
+        print(name)
+        if isinstance(name, str) and name.isalpha():
+            self._name = name
+        else:
+            raise ValueError('Некорректное имя')
+
+    name = property(get_name, set_name)
+
+    #Decoration property
+
+    @property
+    def color(self):                                     # геттер свойства color
+        return self._color
+
+    @color.setter
+    def color(self, color):                               # сеттер свойства color
+        if isinstance(color, str) and color.isalpha():
+            self._color = color
+        else:
+            raise ValueError('Некорректное имя')
+
+    @color.deleter
+    def color(self):                                     # делитер свойства color
+        del self._color
+
+    @classmethod
+    def get_description(cls) -> str:
+        print("Описание класса:", str(cls.description))
 
     def move(self, environment: str='earth') -> str:
         print(f'{self.name} is moving')
 
     def __str__(self) -> str:
-        return f'Животное по имени {self.name}'
+        return f'{self._class_name} по имени: {self.name}'
 
     def __repr__(self) -> str:
-        return f'Animal({self.name, self.age})'
+        return f'{self._class_type}({self.name, self.age})'
 
 
-class Birds(Animals, Fly):
+class Birds(Animals, Fly, Jump):
     description: str = "Птицы"
 
     def __init__(self, name, age):
         Animals.__init__(self, name, age)
         self.legs: int = 2
+        self._class_name: str = 'Птица'
+        self._class_type: str = 'Birds'
 
     def move(self, environment: str = 'air') -> str:
         if environment == 'air':
-            return self.flying()
+            return self.fly()
         elif environment == 'earth':
-            return self.run()
-
-    def flying(self) -> str:
-        print(f"{self.name} is fly")
-
-    def __str__(self) -> str:
-        return f'Птица по имени {self.name}'
-
-    def __repr__(self) ->str:
-        return f'Birds({self.name, self.age})'
-
+            return self.jump()
 
 class Mammals(Animals, Run, Jump):
     description: str = "Млекопитающие"
 
+    def __init__(self, name, age):
+        Animals.__init__(self, name, age)
+        self._class_name: str = 'Млекопитающие'
+        self._class_type: str = 'Mammals'
     def move(self, environment='air') -> str:
         if environment == 'air':
             return self.jump()
         elif environment == 'earth':
             return self.run()
-
-    def __str__(self) -> str:
-        return f'Млекопетающие по имени {self.name}'
-
-    def __repr__(self) -> str:
-        return f'Mammals({self.name, self.age})'
 
 class Griffin(Birds, Mammals):
     description: str = "Это мифическое животное наполовину орел наполовину лев"
@@ -82,14 +108,12 @@ class Griffin(Birds, Mammals):
     def __init__(self, name: str, age: str) -> None:
         Birds.__init__(self, name, age)
         self.legs: int = 4
-    def __str__(self) -> str:
-        return f'Грифон {self.name}'
+        self._class_name: str = 'Гриффон'
+        self._class_type: str = 'Griffin'
 
-    def __repr__(self) -> str:
-        return f'Griffin({self.name, self.age})'
 
 #New class Eagle, Cat, White_Griffin
-
+#Extended class
 class Eagle(Birds):
 
     description = 'Гордый крылатый хишник с мошным клювом и отличным зрением - Орел'
@@ -97,21 +121,17 @@ class Eagle(Birds):
     def __init__(self, name, age) -> None:
         Birds.__init__(self, name, age)
         self.color = 'Broun'
-    def __str__(self)  -> str:
-        return f'Орел по имени {self.name}'
-
-    def __repr__(self)  -> str:
-        return f'Eagle({self.name, self.age})'
+        self._class_name: str = 'Орел'
+        self._class_type: str = 'Eagle'
 
 class Cat(Mammals):
 
     description = 'Чудный мелкий пушистик спасающтий людей от грызунов - Кот'
 
-    def __str__(self)   -> str:
-        return f'Кот по имени {self.name}'
-
-    def __repr__(self)  -> str:
-        return f'Cat({self.name, self.age})'
+    def __init__(self, name, age) -> None:
+        Birds.__init__(self, name, age)
+        self._class_name: str = 'Кот'
+        self._class_type: str = 'Cat'
 
 class White_Griffin(Griffin):
 
@@ -121,12 +141,9 @@ class White_Griffin(Griffin):
         Birds.__init__(self, name, age)
         self.legs: int = 4
         self.color: str = 'White'
+        self._class_name: str = 'Белый Гриффон'
+        self._class_type: str = 'White_Griffin'
 
-    def __str__(self)  -> str:
-        return f'Белый гриффон по имени {self.name}'
-
-    def __repr__(self) -> str:
-        return f'White_Griffin({self.name, self.age})'
 
 
 lst = [
@@ -141,4 +158,12 @@ lst = [
 
 for animal in lst:
     print(animal)
-    print(repr(animal), end='\n\n')
+    animal.get_description()
+    print(repr(animal))
+    animal.move()
+    animal.move(environment='earth')
+    animal.move(environment='air')
+    print('-' * 15)
+    __import__('time').sleep(1)
+
+
